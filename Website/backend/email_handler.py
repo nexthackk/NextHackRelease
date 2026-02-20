7a2c4488a6d0f9b63122975ffb1a4b645bacd241
nexthackk <contactnexthack@gmail.com>
2026-02-20 20:34:35 +0530

    Update email_handler.py

--- Website/backend/email_handler.py
+++ Website/backend/email_handler.py
@@ -279,6 +279,3 @@ async def list_subscribers(limit: int = 100, offset: int = 0):
 # Run Server
 # ======================================================
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
